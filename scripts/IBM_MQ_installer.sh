fc5fdf3b3cdc59a09bacad6751d1d759c75396ac
Zeke Kaufman <zeke.kaufman@gmail.com>
2020-05-18 13:04:34 -0400

    User and group setup.

--- scripts/IBM_MQ_installer.sh
+++ scripts/IBM_MQ_installer.sh
@@ -37,6 +37,36 @@ fi
 
 
 ###################################
+## Perform various setup and group assignments
+## for the mqm user and the opc user.
+###################################
+## Set password for mqm user
+echo Ibmmq123! | sudo passwd mqm --stdin
+
+## Add opc to the mqm group
+usermod -g mqm opc
+
+
+###################################
+## Implement passwordless ssh:
+## 
+##   https://www.ibm.com/support/knowledgecenter/SSFKSJ_9.1.0/com.ibm.mq.con.doc/q131120_.htm
+##
+## This is goinf to require a post installation/instance creation
+## script run as the mqm user on all 3 nodes.
+###################################
+# ssh-keygen -t rsa -f /home/mqm/.ssh/id_rsa -N ''
+# ssh-copy-id -i /home/mqm/.ssh/id_rsa.pub remote_node1_primary_address
+# ssh-copy-id -i /home/mqm/.ssh/id_rsa.pub remote_node1_alternate_address
+# ssh-copy-id -i /home/mqm/.ssh/id_rsa.pub remote_node2_primary_address
+# ssh-copy-id -i /home/mqm/.ssh/id_rsa.pub remote_node2_alternate_address
+# ssh remote_node1_primary_address uname -n
+# ssh remote_node1_alternate_address uname -n
+# ssh remote_node2_primary_address uname -n
+# ssh remote_node2_alternate_address uname -n
+
+
+###################################
 ## Verify server-to-server installation
 ###################################
 
