c23e991cee9f135da4d7e2f6a783b1041b509546
Zeke Kaufman <zeke.kaufman@gmail.com>
2020-09-09 17:16:11 -0400

    Get rid of confusing OS name filter.

--- data_sources.tf
+++ data_sources.tf
@@ -5,13 +5,6 @@ data "oci_core_images" "ol7" {
   sort_order       = "DESC"
   state            = "AVAILABLE"
 
-  # filter restricts to pegged version regardless of region
-  filter {
-    name   = "display_name"
-    values = ["Oracle-Linux-7.5-2018.08.14-0"]
-    regex  = false
-  }
-
   # filter restricts to OL 7
   filter {
     name   = "operating_system_version"
