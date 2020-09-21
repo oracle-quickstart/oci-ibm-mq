47efa6fad116e64387c4bb654cf3ac98aaa8ace4
Zeke Kaufman <zeke.kaufman@gmail.com>
2020-09-21 19:19:30 -0400

    Fix image reference, mount command, and runuser->sudo

--- scripts/configure.sh
+++ scripts/configure.sh
@@ -1,13 +1,14 @@
 set -x
 
-export MQ_INSTALL_NUMBER=2
-export SERVER_MOUNT_DIR=/mnt/nfsshare/exports
+export MQ_NAME="MQ1"
+export NFS_SERVER_IP="10.0.3.200"
+export SERVER_MOUNT_DIR=/
 export CLIENT_MOUNT_DIR=/mnt/nfsv4
 
 function mount_nfs_server {
   yum -y install nfs-utils
   mkdir -p ${CLIENT_MOUNT_DIR}
-  mount -t nfs -o hard,intr 10.0.3.3:${SERVER_MOUNT_DIR} ${CLIENT_MOUNT_DIR}
+  mount -t nfs -o vers=4,defaults,noatime,bg,timeo=100,ac,actimeo=120,nocto,rsize=1048576,wsize=1048576,nolock,local_lock=none,proto=tcp,sec=sys,_netdev ${NFS_SERVER_IP}:${SERVER_MOUNT_DIR} ${CLIENT_MOUNT_DIR} 
 }
 
 ## https://www.ibm.com/support/knowledgecenter/en/SSFKSJ_9.1.0/com.ibm.mq.ins.doc/q008640_.htm
@@ -32,7 +33,7 @@ function create_queue_manager {
   mkdir ${CLIENT_MOUNT_DIR}/MQHA${MQ_INSTALL_NUMBER}
   
   ln -s ${CLIENT_MOUNT_DIR}/MQHA${MQ_INSTALL_NUMBER} /MQHA
-  
+
   if [ ! -d /MQHA/logs ]; then
     mkdir /MQHA/logs
   fi
@@ -44,22 +45,20 @@ function create_queue_manager {
   chown -R mqm:mqm /MQHA/
   chmod -R ug+rwx /MQHA/
 
-  sleep 15
   if [[ `hostname` == *0 ]]; then
-    runuser -l mqm -c '. /opt/mqm/bin/setmqenv -s ;  crtmqm -ld /MQHA/logs -md /MQHA/qmgrs QM1'
+    sudo -E -u mqm bash -c '. /opt/mqm/bin/setmqenv -s ;  crtmqm -ld /MQHA/logs -md /MQHA/qmgrs $MQ_NAME'
+    sleep 10
   elif [[ `hostname` == *1 ]]; then
-    runuser -l mqm -c '. /opt/mqm/bin/setmqenv -s ; addmqinf -s QueueManager -v Name=QM1 -v Directory=QM1 -v Prefix=/var/mqm -v DataPath=/MQHA/qmgrs/QM1'
+    sleep 10
+    sudo -E -u mqm bash -c '. /opt/mqm/bin/setmqenv -s ; addmqinf -s QueueManager -v Name=$MQ_NAME -v Directory=$MQ_NAME -v Prefix=/var/mqm -v DataPath=/MQHA/qmgrs/$MQ_NAME'
   fi
-  sleep 15
-  runuser -l mqm -c '. /opt/mqm/bin/setmqenv -s ; strmqm -x QM1'
+  sudo -E -u mqm bash -c '. /opt/mqm/bin/setmqenv -s ; strmqm -x $MQ_NAME'
 }
-
 
 sudo setenforce 0
 
 systemctl stop firewalld
 systemctl disable firewalld
-
 
 mount_nfs_server
 install_ibmmq
