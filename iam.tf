1c34dc13b0587078414231e854382cfa16f084a4
Collin Poczatek <collin.poczatek@gmail.com>
2020-10-08 11:43:41 -0700

    policy fix, add subscription

--- iam.tf
+++ iam.tf
@@ -19,9 +19,9 @@ resource "oci_identity_policy" "ha_iam_policy" {
   description    = "Allows instances to use vnics, subnets and private-ips APIs"
   compartment_id = var.compartment_ocid
   statements = [
-    "Allow dynamic-group ${oci_identity_dynamic_group.ha_dynamic_group.name} to use private-ips in compartment ${var.compartment_ocid}",
-    "Allow dynamic-group ${oci_identity_dynamic_group.ha_dynamic_group.name} to use vnics in compartment ${var.compartment_ocid}",
-    "Allow dynamic-group ${oci_identity_dynamic_group.ha_dynamic_group.name} to use subnets in compartment ${var.compartment_ocid}"
+    "Allow dynamic-group ${oci_identity_dynamic_group.ha_dynamic_group.name} to use private-ips in compartment id ${var.compartment_ocid}",
+    "Allow dynamic-group ${oci_identity_dynamic_group.ha_dynamic_group.name} to use vnics in compartment id ${var.compartment_ocid}",
+    "Allow dynamic-group ${oci_identity_dynamic_group.ha_dynamic_group.name} to use subnets in compartment id ${var.compartment_ocid}"
   ]
 
 }
